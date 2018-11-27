f275d1baba0210f29b00f3023728e17b546afa21
GillAlexander <jonte962010@hotmail.com>
2018-11-27 13:53:35 +0100

    Initial commit
    Hej

---
+++ Assets/Scripts/switchBetweenCameras.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchBetweenCameras : MonoBehaviour {
+
+    public Camera playerCamera;
+
+    public Camera elioCamera;
+    
+	// Use this for initialization
+	void Start () {
+        playerCamera.gameObject.SetActive(true);
+        elioCamera.gameObject.SetActive(false);
+
+        elioCamera.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            playerCamera.gameObject.SetActive(false);
+            elioCamera.gameObject.SetActive(true);
+
+        }
+        else
+        {
+            playerCamera.gameObject.SetActive(true);
+            elioCamera.gameObject.SetActive(false);
+        }
+	}
+}
