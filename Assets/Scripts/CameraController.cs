f0376d5a251cf4f27440f40adf3b4162c61300ae
GillAlexander <jonte962010@hotmail.com>
2018-11-27 13:53:35 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class CameraController : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 xboxLook;
+
+    public float sensitivity;
+
+    GameObject character; 
+
+	// Use this for initialization
+	void Start () {
+        character = this.transform.parent.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector2 cameraChange = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 xboxCamera = new Vector2(Input.GetAxis("xboxX"), Input.GetAxisRaw("xboxY"));
+
+        //cameraChange = Vector2.Scale(cameraChange, new Vector2(sensitivity, sensitivity));
+        xboxCamera = Vector2.Scale(xboxCamera, new Vector2(sensitivity, sensitivity));
+
+        //mouseLook += cameraChange;
+        //mouseLook.y = Mathf.Clamp(mouseLook.y, -90, 90);
+
+        xboxLook += xboxCamera;
+        xboxLook.y = Mathf.Clamp(xboxCamera.y, -90, 90);
+
+        //transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        //character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+        transform.localRotation = Quaternion.AngleAxis(-xboxLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(xboxLook.x, character.transform.up);
+
+
+        //character.transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Right.X * 25.0f * Time.deltaTime, 0.0f);
+        //transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Right.Y * 25.0f * Time.deltaTime, 0.0f);
+       
+    }
+}
---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Playercontroller : MonoBehaviour {
+
+    public float speed;
+    Transform position;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        position = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+
+        moveVertical *= Time.deltaTime;
+        moveHorizontal *= Time.deltaTime;
+
+        transform.Translate(moveHorizontal, 0, moveVertical);
+
+        //float xboxVertical = Input.GetAxisRaw("Vertical") * speed;
+        //float xboxHorizontal = Input.GetAxisRaw("Horizontal") * speed;
+        //xboxVertical *= Time.deltaTime;
+        //xboxHorizontal *= Time.deltaTime;
+        //transform.Translate(xboxHorizontal, 0, xboxVertical);
+    }
+}
