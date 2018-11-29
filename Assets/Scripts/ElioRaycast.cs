e4d6adc7abb7e309be78d43ed08be3e2bd04cc05
BlitzBurn <JK98@Live.se>
2018-11-29 19:58:42 +0100

    Added early RayCast Prototype

---
+++ Assets/Scripts/ElioRaycast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElioRaycast : MonoBehaviour {
+
+    int layerMask = 1 << 9;
+    public float ElioVisionRange;
+    RaycastHit hit;
+    public static bool PlayerDetected = false;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update () {
+      
+
+        for (float i=0; i<720; i++)
+        {
+            float ElioRayX = ElioVisionRange * Mathf.Cos((1/2) * i);
+            float ElioRayY = ElioVisionRange * Mathf.Sin((1/2) * i);
+
+            Physics.Raycast(transform.position, transform.TransformDirection(ElioRayX, 0, ElioRayY), out hit, ElioVisionRange, layerMask);
+            if (hit.collider.tag == "capsule")
+            {
+                PlayerDetected = true;
+            }
+            else
+            {
+                PlayerDetected = false;
+            }
+
+        }
+
+	}
+}
