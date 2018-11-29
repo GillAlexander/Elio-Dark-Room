74b2dc8a0c80f805ccfa5ac6a5084e9640a7dbc8
GillAlexander <jonte962010@hotmail.com>
2018-11-29 21:05:59 +0100

    new input manager

---
+++ Assets/Scripts/InputManager/JonathanPlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class JonathanPlayerController : MonoBehaviour
+{
+    public float speed;
+    public float sprint;
+
+    void Update()
+    {
+        //float moveVertical = Input.GetAxis("Vertical") * speed;
+        //float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+
+        //moveVertical *= Time.deltaTime;
+        //moveHorizontal *= Time.deltaTime;
+
+        
+
+        //transform.Translate(moveHorizontal, 0, moveVertical);
+
+        float moveVertical = InputManager.MainVertical() * speed;
+        float moveHorizontal = InputManager.MainHorizontal() * speed;
+
+        moveVertical *= Time.deltaTime;
+        moveHorizontal *= Time.deltaTime;
+
+        transform.Translate(moveHorizontal, 0, moveVertical);
+
+        if (InputManager.SprintButton())
+        {
+            moveVertical *= sprint;
+            moveHorizontal *= sprint;
+        }
+
+
+        if (Input.GetKey("joystick button 4") || Input.GetButton("Sprint"))
+        {
+            moveVertical *= sprint;
+            moveHorizontal *= sprint;
+        }
+    }
+}
