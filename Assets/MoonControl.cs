d2e2d0a6e9327945be0a7fbcf627cf4fc93d52b5
Philip <philipa@hotmail.se>
2018-12-03 12:19:59 +0100

    Day and night cycle

---
+++ Assets/MoonControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoonControl : MonoBehaviour
+{
+    public float scale = 15.0f;
+    // Use this for initialization
+    void Start()
+    {
+        transform.localScale = new Vector3 (scale, scale, scale);
+    }
+}
