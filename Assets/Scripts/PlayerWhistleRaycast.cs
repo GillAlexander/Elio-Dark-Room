d324bd24d71f990afad319552bf4441122ee3442
BlitzBurn <39604601+BlitzBurn@users.noreply.github.com>
2018-12-06 13:34:22 +0100

    Prototype whistle mechanic

---
+++ Assets/Scripts/PlayerWhistleRaycast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWhistleRaycast : MonoBehaviour {
+    public float WhistleRange;
+    float EchoDistance;
+    RaycastHit contact;
+
+    
+    void Start () {
+      
+	}
+	
+
+	void Update () {
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out contact, WhistleRange))
+        {
+            EchoDistance = contact.distance;
+            Debug.Log("Player sees something: "+EchoDistance);
+        }
+        Debug.DrawRay(transform.position, contact.point - transform.position, Color.blue);
+    }
+}
