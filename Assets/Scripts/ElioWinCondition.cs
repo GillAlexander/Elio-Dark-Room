329f31de9513628bf35520870b733f0f03d7f795
BlitzBurn <39604601+BlitzBurn@users.noreply.github.com>
2018-12-14 11:59:29 +0100

    ElioWinConditionUpdated

---
+++ Assets/Scripts/ElioWinCondition.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElioWinCondition : MonoBehaviour {
+    public GameObject elio;
+    public GameObject player;
+    public bool foundElio;
+    public GameObject[] ElioHidingSpot;
+    float distance;
+    float passedTime;
+    private void Start()
+    {
+        //foundElio = false;
+    }
+
+    private void Update()
+    {
+
+        
+
+        passedTime += Time.deltaTime;
+
+        if (foundElio == true&&passedTime>=5)
+        {
+            StartCoroutine(elioFoundReset());
+        }
+    }
+
+    private void OnTriggerEnter(Collider spheres)
+    {
+        
+
+        if (spheres.gameObject.name == ("Player") && passedTime>=5)
+        {
+            Debug.Log("You found Elio! You won!");
+            foundElio = true;
+            PlayerNoise.foundElio = true;
+            distance = Vector3.Distance(elio.transform.position, player.transform.position);
+
+            while (foundElio && distance < 10)
+            {
+                
+                elio.transform.position =
+                ElioHidingSpot[Random.Range(0, ElioHidingSpot.Length)].transform.position;
+                distance = Vector3.Distance(elio.transform.position, player.transform.position);
+                //Debug.Log(distance);
+            }
+            passedTime = 0;
+        }
+
+        
+
+    }
+
+    IEnumerator elioFoundReset()
+    {
+        Debug.Log("elioFoundResetActivated");
+        yield return new WaitForSeconds(2);
+        foundElio = false;
+        Debug.Log("foundElioResetCompleted");
+
+    }
+
+}
+//först hittar nytt randomiserat gömställe.
+//kolla sen ifall det är tillräckligt långt bort från spelaren. 
+
+//current/active hidingspot. 
+//while distance to player (meter)
+//randomisera ny hiding spot
+//while distance längre än 50 m = true
---
+++ Assets/Scripts/PointSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour {
+    ElioWinCondition Eliofound;
+    int gameScore = 0;
+	
+	void Start () {
+        Eliofound = GameObject.Find("Elio").GetComponent<ElioWinCondition>();
+    }
+	
+
+	void Update () {
+
+        Debug.Log(Eliofound.foundElio);
+        Debug.Log("pointSystemUpdate");
+
+        if (Eliofound.foundElio == true)
+        {
+            gameScore = gameScore + 1;
+            Debug.Log("Gamescore: "+gameScore);
+            Debug.Log("Elio found true");
+        }
+        else if (Eliofound.foundElio == false)
+        {
+            Debug.Log("Elio found false");
+        }
+	}
+}
