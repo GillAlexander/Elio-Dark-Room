e48a21954fedec6a30e8b247c8fca89f71076c4d
Draparn <beatboy80@hotmail.com>
2018-12-19 18:07:51 +0100

    Replaced placeholder bird sound file with new ones to be implemented. Also worked the sound mixer some.

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public AudioClip[] birdCalls;
+    public AudioSource source;
+
+    void Start()
+    {
+        StartCoroutine(Chirp());
+        source = GetComponent<AudioSource>();
+    }
+
+    IEnumerator Chirp()
+    {
+        if (birdCalls.Length < 2)
+            yield return new WaitForSeconds(Random.Range(9, 15));
+        else
+            yield return new WaitForSeconds(Random.Range(3, 7));
+
+        source.clip = birdCalls[Random.Range(0, birdCalls.Length)];
+        source.pitch = Random.Range(0.95f, 1.05f);
+        source.Play();
+        StartCoroutine(Chirp());
+    }
+}
