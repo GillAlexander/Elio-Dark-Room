8dc9c98652af2a614ff6904a69e7e32db1ec3a30
Philip <philipa@hotmail.se>
2019-01-04 13:48:33 +0100

    funny up deer

---
+++ Assets/Scripts/RaycastObstaclesAvoid.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastObstaclesAvoid : MonoBehaviour
+{
+    // Fix a range how early u want your enemy detect the obstacle.
+    private int range;
+    private float speed;
+    private bool isThereAnyThing = false;
+    // Specify the target for the enemy.
+    GameObject target;
+    private float rotationSpeed;
+    private RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+        range = 80;
+        speed = 2f;
+        rotationSpeed = 5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Look At Somthly Towards the Target if there is nothing in front.
+        if (!isThereAnyThing)
+        {
+            Vector3 relativePos = target.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        }
+        //Checking for any Obstacle in front.
+        // Two rays left and right to the object to detect the obstacle.
+        Transform leftRay = transform;
+        Transform rightRay = transform;
+        //Use Phyics.RayCast to detect the obstacle
+        if (Physics.Raycast(leftRay.position + (transform.right * 7), transform.forward, out hit, range) || Physics.Raycast(rightRay.position - (transform.right * 7), transform.forward, out hit, range))
+        {
+            if (hit.collider.gameObject.CompareTag("Obstacle"))
+            {
+                isThereAnyThing = true;
+                transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+            }
+        }
+        // Now Two More RayCast At The End of Object to detect that object has already pass the obsatacle.
+        // Just making this boolean variable false it means there is nothing in front of object.
+        if (Physics.Raycast(transform.position - (transform.forward * 4), transform.right, out hit, 10) ||
+         Physics.Raycast(transform.position - (transform.forward * 4), -transform.right, out hit, 10))
+        {
+            if (hit.collider.gameObject.CompareTag("Obstacle"))
+            {
+                isThereAnyThing = false;
+            }
+        }
+        // Use to debug the Physics.RayCast.
+        Debug.DrawRay(transform.position + (transform.right * 7), transform.forward * 20, Color.red);
+        Debug.DrawRay(transform.position - (transform.right * 7), transform.forward * 20, Color.red);
+        Debug.DrawRay(transform.position - (transform.forward * 4), -transform.right * 20, Color.yellow);
+        Debug.DrawRay(transform.position - (transform.forward * 4), transform.right * 20, Color.yellow);
+    }
+}
