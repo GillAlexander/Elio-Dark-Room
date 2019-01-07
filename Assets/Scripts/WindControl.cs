5bd044ded1a0879520c8f9b33e5376b152e1cbc8
Draparn <beatboy80@hotmail.com>
2019-01-07 14:35:05 +0100

    Added wind control for when you enter the snowy area and changed Player and Elio Prefabs some.

---
+++ Assets/Scripts/WindControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class WindControl : MonoBehaviour
+{
+    public AudioMixer mixer;
+    bool snowSwap;
+    bool onSnow = false;
+    float windChange = -5;
+
+    void Update()
+    {
+        if (PlayerFootsteps.surfaceTag == "Snow")
+        {
+            snowSwap = onSnow;
+            onSnow = true;
+        }
+        else
+        {
+            snowSwap = onSnow;
+            onSnow = false;
+        }
+
+        if (onSnow != snowSwap)
+        {
+            StartCoroutine(WindFade());
+        }
+
+    }
+
+    IEnumerator WindFade()
+    {
+        mixer.SetFloat("WindParam", windChange);
+        if (onSnow && windChange < 4)
+        {
+            windChange += 0.2f;
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(WindFade());
+        }
+
+        else if (!onSnow && windChange > -5)
+        {
+            windChange -= 0.2f;
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(WindFade());
+        }
+    }
+}
