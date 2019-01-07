f308f2c4d099a41cb71626767d0f0d736070bc5e
amiefaily <amie.faily@hotmail.com>
2019-01-07 14:51:29 +0100

    menykoppladtillspel

--- Assets/Scripts/MainMenunewww.cs
+++ Assets/Scripts/MainMenunewww.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenunewww : MonoBehaviour {
+public class MainMenunewww : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Elio");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("AVSLUTA!");
+        Application.Quit();
+    }
+
 }
+
