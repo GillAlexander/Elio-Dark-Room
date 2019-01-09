8cad545e7b4d65f3d29f4f4a040f2ed9b68b99dd
Draparn <beatboy80@hotmail.com>
2019-01-09 11:26:48 +0100

    Added music fade-in to menu and tweaked the sound mixer levels

---
+++ Assets/Scripts/MusicVolumeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicVolumeControl : MonoBehaviour
+{
+    public AudioMixer mixer;
+    float volume;
+
+    void Start()
+    {
+        volume = -60;
+        StartCoroutine(MusicFade());
+    }
+
+    IEnumerator MusicFade()
+    {
+        mixer.SetFloat("MusicParam", volume);
+        volume += 1.5f;
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (volume < -29)
+            StartCoroutine(MusicFade());
+    }
+}
