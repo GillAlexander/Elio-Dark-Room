1e715425d0fa3bca6a0ff98ff8e35314ee6690d9
Draparn <beatboy80@hotmail.com>
2019-01-09 12:15:10 +0100

    Added code to play sound when you mouse over a button in menu

---
+++ Assets/MouseOver.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip[] choices;
+    public static string select = "";
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        switch (select)
+        {
+            case "Start":
+                source.clip = choices[1];
+                source.Play();
+                break;
+
+            case "About":
+                source.clip = choices[2];
+                source.Play();
+                break;
+
+            case "Settings":
+                source.clip = choices[3];
+                source.Play();
+                break;
+
+            case "Exit":
+                source.clip = choices[4];
+                source.Play();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
